01b8b55fe88de7dbdde8af05ded6ceb871069688
Manuel Cuello <mcuello@c4r3s2.42urduliz.com>
2024-11-28 18:13:21 +0100

    adding libft

---
+++ libft/ft_strlcpy.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcuello <mcuello@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/28 15:41:46 by mcuello           #+#    #+#             */
+/*   Updated: 2024/11/28 16:16:04 by mcuello          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	counter;
+
+	i = 0;
+	counter = 0;
+	if (!dest || !src)
+		return (0);
+	while (src[counter])
+		counter++;
+	while (src[i] && i + 1 < size)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (size)
+		dest[i] = '\0';
+	return (counter);
+}
