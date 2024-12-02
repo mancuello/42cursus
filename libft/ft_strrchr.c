20e0b5c7f86435f76255c245d06d2cfbf0ff30bb
Manu_ZenBook <mcuello7743@gmail.com>
2024-12-02 12:02:47 +0100

    adding some

---
+++ libft/ft_strrchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mcuello <mcuello@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/30 19:05:37 by mcuello           #+#    #+#             */
+/*   Updated: 2024/11/30 19:05:37 by mcuello          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(cons char *s, int c)
+{
+	int	last;
+
+	last = ft_strlen(str) + 1;
+	while (last--)
+	{
+		if (s[last] == c)
+			return (s + last);
+	}
+	return (NULL);
+}
+ 
