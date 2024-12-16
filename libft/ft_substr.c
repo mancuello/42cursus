23a3f73cf619a51c7e99d6e17b3a74f441df8e23
Manuel Cuello <mcuello@c4r3s3.42urduliz.com>
2024-12-16 19:34:23 +0100

    fixing ft_substr

--- libft/ft_substr.c
+++ libft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mcuello <mcuello@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 15:06:56 by mcuello           #+#    #+#             */
-/*   Updated: 2024/12/16 17:30:14 by mcuello          ###   ########.fr       */
+/*   Updated: 2024/12/16 19:29:40 by mcuello          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	src_len = ft_strlen(s);
 	if (start >= src_len)
 		return (ft_strdup(""));
-	sub = malloc((len + 1) * sizeof(char));
-	if (!sub)
-		return (NULL);
 	if (len > src_len - start)
 		len = src_len - start;
+	sub = malloc(len + 1);
+	if (!sub)
+		return (NULL);
 	i = 0;
 	while (i < len)
 	{
@@ -38,52 +38,3 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (sub);
 }
 
-/* #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stddef.h>
-
-int main(void)
-{
-    char *original = "Hello, 42 Students!";
-    char *substring;
-
-    // Caso 1: Extraer una parte del medio
-    substring = ft_substr(original, 17, 20);
-    if (substring)
-    {
-        printf("Caso 1 - Resultado esperado: \"nts!\"\n");
-        printf("Caso 1 - Resultado obtenido: \"%s\"\n", substring);
-        free(substring);
-    }
-
-    // Caso 2: Extraer desde el inicio
-    substring = ft_substr(original, 0, 5);
-    if (substring)
-    {
-        printf("\nCaso 2 - Resultado esperado: \"Hello\"\n");
-        printf("Caso 2 - Resultado obtenido: \"%s\"\n", substring);
-        free(substring);
-    }
-
-    // Caso 3: Extraer más allá de la longitud del string
-    substring = ft_substr(original, 15, 10);
-    if (substring)
-    {
-        printf("\nCaso 3 - Resultado esperado: \"nts!\"\n");
-        printf("Caso 3 - Resultado obtenido: \"%s\"\n", substring);
-        free(substring);
-    }
-
-    // Caso 4: Empezar después de la longitud de la cadena original
-    substring = ft_substr(original, 25, 5);
-    if (substring)
-    {
-        printf("\nCaso 4 - Resultado esperado: \"\"\n");
-        printf("Caso 4 - Resultado obtenido: \"%s\"\n", substring);
-        free(substring);
-    }
-	else 
-		printf("no entro al caso 4\n");
-    return 0;
-} */
