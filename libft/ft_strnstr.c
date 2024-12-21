bca60b0e0080d9c88391f07b7d8a3397b66b0387
Manu_ZenBook <mcuello7743@gmail.com>
2024-12-21 22:54:59 +0100

    fixing strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -20,34 +20,41 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	if (!*little)
 		return ((char *)big);
 	i = 0;
-	j = 0;
-	while (big[i] && i < len && little[j])
+	while (big[i] && i < len)
 	{
-		while (big[i + j] == little[j] && i + j < len)
+	    j = 0;
+		while ((little[i] != '\0') && (big[i + j] == little[j]) && (i + j) < len)
 			j++;
 		if (!little[j])
 			return ((char *)(big + i));
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
 
 //Linea 26: Analiza si llegó al final de la cadena, indicador
 //de que recorrió todo el string.
 
-/* #include <stdio.h>
-#include <string.h>
-
-int main() {
+/* int main()
+{
     const char big[] = "Hello, world!";
     const char little[] = "world";
     char *result;
 
-    result = ft_strnstr(big, little, 10);  // Busca en los primeros 10 caracteres
+    result = ft_strnstr(big, little, 12);
     if (result)
         printf("Subcadena encontrada: %s\n", result);
     else
         printf("Subcadena no encontrada.\n");
 
     return 0;
+} */
+/* 
+int main(void)
+{
+    char *s = "abc";
+    
+    printf("El resultado mio es: %s\n", ft_strnstr("ab abc", "bcc", 3));
+    printf("El resultado original es: %s\n", strnstr(s, s, 3));
+    return (0);
 } */
